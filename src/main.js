9654e5dbfebd2c60ec1b5dc6ac680d79b279c002
John <mktkamehouse@gmail.com>
2022-12-24 01:28:42 -0300

    criando seção FAQ

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
 
     for(let i = 0; i < buttons.length; i++){
@@ -14,7 +15,18 @@ document.addEventListener('DOMContentLoaded', function(){
 
         })
     }
+
+    for(let i=0 ; i < questions.length; i++){
+        questions[i].addEventListener('click', openOrCloseQuestion)
+    }
 })
+
+function openOrCloseQuestion(elemento){
+    const classe = 'faq__questions__item--is-open;';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle('faq__questions__item--is-open')
+}
 
 function hideAlltabs(){
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
